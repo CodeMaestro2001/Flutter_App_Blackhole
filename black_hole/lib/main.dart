b9d66f9af37502ba78610262e62edc55d45783ad
Tharusha <156021977+CodeMaestro2001@users.noreply.github.com>
2024-12-25 20:03:35 +0530

    add row widget and 2 nd page done

--- black_hole/lib/main.dart
+++ black_hole/lib/main.dart
@@ -76,16 +76,94 @@ class MyApp extends StatelessWidget {
                 ),
 
                 //button
-                Center(
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      borderRadius:BorderRadius.circular(100),
-                      color: Colors.red
-                     ),
-                    child: Text("Space details",style:TextStyle(color: Colors.white),),
+                GestureDetector(
+                  onTap: () {},
+                  child: Center(
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius:BorderRadius.circular(100),
+                        color: Colors.red
+                       ),
+                      child: Text("Space details",style:TextStyle(color: Colors.white),),
+                    ),
                   ),
-                )
+                ),
+
+              //second screen 
+              Center(
+                child: Image.asset(
+                  "assets/space2.png",
+                  height: 250,
+                  scale: 2,
+                  )
+                  ),
+
+                 SizedBox(
+                    height:50
+                  ),
+
+              Text("A black hole is a region of space where gravity is so strong that nothing, not even light, can escape from it. This intense gravitational pull is caused by the collapse of a massive star at the end of its life cycle. Black holes are invisible, but their presence can be detected by the effects they have on nearby matter and light. They come in different sizes, from small stellar black holes formed by dying stars to supermassive black holes found at the centers of galaxiesThey come in different sizes, from small stellar black holes formed by dying stars to supermassive black holes found at the centers of galaxies.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w300
+              ),
+              ),
+
+              SizedBox(
+                    height:50
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.blue
+                      ),
+                
+                      
+                    ),
+                
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.red
+                      ),
+                    ),
+                
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.green
+                      ),
+                    ),
+                
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.yellow
+                      ),
+                    ),
+                
+                  ],
+                ),
+              )
+
+
               ],
             ),
           ),
